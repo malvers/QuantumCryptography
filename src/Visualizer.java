d7993cb3a1952fd2af0abdc5763a5641abdee784
Dr. Michael R. Alvers <m.alvers@ganata.ownprovider.com>
2023-12-02 13:34:27 +0100

    quantum rocks

---
+++ src/Visualizer.java
@@ -0,0 +1,125 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Visualizer extends JButton {
+
+    private BufferedImage image;
+
+    public Visualizer() {
+
+        loadImage();
+
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                handleKeyPress(e);
+            }
+        });
+    }
+
+    private void loadImage() {
+        try {
+            image = ImageIO.read(new File("universe.png"));
+        } catch (IOException e) {
+            System.out.println("clouds not found :-/");
+        }
+    }
+
+    @Override
+    public void paint(Graphics g) {
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        drawImage(g2d);
+
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    }
+
+    private void drawImage(Graphics2D g2d) {
+
+        if (image == null) {
+            return;
+        }
+//        float alpha = 0.10f; // Adjust this value between 0.0f (fully transparent) and 1.0f (fully opaque)
+//        AlphaComposite alphaComposite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha);
+//        g2d.setComposite(alphaComposite);
+
+        g2d.drawImage(image, 0, 0, getWidth(), getHeight(), Color.BLACK, this);
+
+//        alpha = 1.0f; // Adjust this value between 0.0f (fully transparent) and 1.0f (fully opaque)
+//        alphaComposite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha);
+//        g2d.setComposite(alphaComposite);
+    }
+
+    private void handleKeyPress(KeyEvent e) {
+
+        switch (e.getKeyCode()) {
+
+            case KeyEvent.VK_SPACE:
+                break;
+            case KeyEvent.VK_ENTER:
+                break;
+            case KeyEvent.VK_UP:
+                break;
+            case KeyEvent.VK_DOWN:
+                break;
+            case KeyEvent.VK_LEFT:
+            case KeyEvent.VK_RIGHT:
+                break;
+
+            /// letter keys ////////////////////////////////////////////////////////////////////////////////////////////
+            case KeyEvent.VK_D:
+                break;
+            case KeyEvent.VK_H:
+                break;
+            case KeyEvent.VK_I:
+                break;
+            case KeyEvent.VK_P:
+                break;
+            case KeyEvent.VK_T:
+                break;
+            case KeyEvent.VK_ESCAPE:
+            case KeyEvent.VK_W:
+                System.exit(0);
+                break;
+        }
+        repaint();
+
+    }
+
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> {
+
+            JFrame f = new JFrame();
+            Visualizer v = new Visualizer();
+            f.add(v);
+            int height = 800;
+            f.addWindowListener(new java.awt.event.WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent windowEvent) {
+
+                }
+
+                @Override
+                public void windowClosed(WindowEvent windowEvent) {
+                    // This will be called when the window is closed
+                    System.out.println("Window is closed");
+                    // Add your custom logic here
+                }
+            });
+            f.setSize((int) (height * 1.618), height);
+            f.setLocation(500, 0);
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            f.setVisible(true);
+        });
+    }
+}
